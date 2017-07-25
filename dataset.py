b37483c1b1099ccf62c2b11b2b771344e5848286
T.Nohara <e@mail.com>
2017-07-25 15:46:00 +0900

    first

---
+++ dataset.py
@@ -0,0 +1,106 @@
+#! coding:utf-8
+"""
+dataset.py
+
+Created by 0160929 on 2017/07/24 8:59
+"""
+from pathlib import Path
+
+import os
+import numpy as np
+
+root_dir = os.path.dirname(os.path.abspath(__file__))
+
+NoSairen_dirpath = Path(root_dir).joinpath("sairen+magna").joinpath("NoSairen")
+OnSairen_dirpath = Path(root_dir).joinpath("sairen+magna").joinpath("OnSairen")
+dataset_filename_X = Path(root_dir).joinpath("sairen+magna").joinpath("sairen-datasets-X")
+dataset_filename_y = Path(root_dir).joinpath("sairen+magna").joinpath( "sairen-datasets-y")
+
+
+def mp3_to_array(file):
+    from pydub import AudioSegment
+
+    print("load :"+file)
+    # MP3からRAWへの変換
+    song = AudioSegment.from_mp3(file)
+
+    # RAWからbytestring型への変換
+    song_data = song._data
+
+    # bytestringからNumpy配列への変換
+    song_arr = np.fromstring(song_data, np.int16)
+
+    return song_arr
+
+
+def load(n=1000):
+
+    npy_X = dataset_filename_X+"-n"+str(n)
+    npy_y = dataset_filename_y+"-n"+str(n)
+
+    if(Path(dataset_filename_X).exists() and Path(dataset_filename_y).exists()):
+        X = np.load(npy_X)
+        y = np.load(npy_y)
+    else:
+        X,y = _load(n,yIsOneHot=True)
+        np.save(npy_X, X)
+        np.save(npy_y, y)
+    return X,y
+
+
+def _load(n=10000, yIsOneHot=True):
+
+    TOTAL_SIZE = int(n/2)
+
+    NoSairen_mp3_files = list(NoSairen_dirpath.glob("*.mp3"))
+    NoSairen_mp3_files = NoSairen_mp3_files[:TOTAL_SIZE]
+    assert len(NoSairen_mp3_files) == n / 2
+
+    OnSairen_mp3_files = list(OnSairen_dirpath.glob("*.mp3"))
+    OnSairen_mp3_files = OnSairen_mp3_files[:TOTAL_SIZE]
+    assert len(OnSairen_mp3_files) == n / 2
+
+
+    N1 = len(NoSairen_mp3_files)
+    N2 = len(OnSairen_mp3_files)
+    N = N1+N2
+    print("NoSairen Number =>" + str(N1))
+    print("OnSairen Number =>" + str(N2))
+    print("Total Number =>" + str(N))
+
+
+    X1_list=list()
+    for file in NoSairen_mp3_files :
+        if file.exists():
+            try:
+                X1_list.append( mp3_to_array(file.as_posix()))
+            except Exception as e:
+                print(e)
+
+    X2_list = list()
+    for  file in OnSairen_mp3_files:
+        if file.exists():
+            try:
+                X2_list.append(mp3_to_array(file.as_posix()))
+            except Exception as e:
+                print(e)
+
+    X_list = X1_list+X2_list
+    X = np.array(X_list)
+
+    assert X.shape[0] == N
+    X = X.reshape(X.shape[0], X.shape[1], 1) # samplenum, featurelen, ch
+
+    if(yIsOneHot):
+        y = np.array([[1,0]]*N1 + [[0,1]]*N2)
+    else:
+        y = np.array([0] * N1 + [1] * N2)
+
+    print(y.shape)
+    return  X,y
+
+def savenp(savefile):
+    pass
+
+if __name__ == '__main__':
+    load()
---
+++ training.py
@@ -0,0 +1,55 @@
+#! coding:utf-8
+"""
+training.py
+
+Created by 0160929 on 2017/07/24 8:59
+"""
+import keras
+from keras.models import Model
+from keras.layers import Dense, Flatten, Input
+from keras.layers import Conv1D, MaxPooling1D
+
+import dataset
+random_state = 42
+
+BATCH_SIZE = 25
+EPOCH = 10
+
+nClass=2
+
+X,y = dataset.load(n=100)
+
+from sklearn.model_selection import train_test_split
+train_X, test_X, train_y, test_y = train_test_split(X, y, test_size=0.2, random_state=random_state)
+
+features = train_X.shape[1]
+
+x_inputs = Input(shape=(features, 1), name='x_inputs')  # (特徴量数, チャネル数)
+x = Conv1D(128, 256, strides=256,padding='valid', activation='relu')(x_inputs)
+x = Conv1D(32, 8, activation='relu')(x)  # (チャネル数, フィルタの長さ )
+x = MaxPooling1D(4)(x)  # （フィルタの長さ）
+x = Conv1D(32, 8, activation='relu')(x)
+x = MaxPooling1D(4)(x)
+x = Conv1D(32, 8, activation='relu')(x)
+x = MaxPooling1D(4)(x)
+x = Conv1D(32, 8, activation='relu')(x)
+x = MaxPooling1D(4)(x)
+x = Flatten()(x)
+x = Dense(100, activation='relu')(x)  # （ユニット数）
+x_outputs = Dense(nClass, activation='sigmoid', name='x_outputs')(x)
+
+model = Model(inputs=x_inputs, outputs=x_outputs)
+model.compile(optimizer='adam',
+              loss='categorical_crossentropy',
+              metrics=['accuracy'])
+
+model.fit(train_X, train_y, batch_size=BATCH_SIZE, epochs=EPOCH)
+
+# モデル評価
+loss, accuracy = model.evaluate(test_X, test_y, verbose=0)
+print("Accuracy = {:.2f}".format(accuracy))
+
+''' pngへ出力 '''
+# from keras.utils.vis_utils import plot_model
+# plot_model(model, to_file="music_only.png", show_shapes=True)
+
