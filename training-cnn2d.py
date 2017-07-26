728260711c99396db61d19bee02ab82980bda2b9
Tomoyuki Nohara a.k.a FiFi <peace098beat.t@gmail.com>
2017-07-27 07:45:45 +0900

    Update training-cnn2d.py

--- training-cnn2d.py
+++ training-cnn2d.py
@@ -4,40 +4,52 @@ training2.py
 
 Created by 0160929 on 2017/07/25 22:41
 """
+
 from __future__ import print_function
+from pathlib import Path
+
 import keras
+import keras.callbacks
 from keras.preprocessing.image import ImageDataGenerator
 from keras.models import Sequential
 from keras.layers import Dense, Dropout, Activation, Flatten
 from keras.layers import Conv2D, MaxPooling2D
 import numpy as np
+import sklearn.metrics
 
-dataset_size = 1000
+Results_dir = Path(__file__).parent.joinpath("result")
+if (not Results_dir.exists()): Results_dir.mkdir()
+
+MODEL_HDF5_NAME = "model.hdf5"
+
+
+# ==================================================== #
+# Global Config
+# ==================================================== #
+dataset_size = 100
 random_state = 21
 batch_size = 32
 num_classes = 2
-epochs = 200
+epochs = 1
 data_augmentation = False
 
-# The data, shuffled and split between train and test sets:
+
+# ==================================================== #
+# Load Datasets
+# ==================================================== #
 import datasets as ds
-
-X, y = ds.get_mell(num=dataset_size)
-
-X = X[:, :, :, np.newaxis]
-
 from sklearn.model_selection import train_test_split
-
+# The data, shuffled and split between train and test sets:
+X, y = ds.get_mell("data-30dBF", num=dataset_size)
+X = X[:, :, :, np.newaxis]
 x_train, x_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=random_state)
-
 print('x_train shape:', x_train.shape)
 print(x_train.shape[0], 'train samples')
 print(x_test.shape[0], 'test samples')
 
-# Convert class vectors to binary class matrices.
-# y_train = keras.utils.to_categorical(y_train, num_classes)
-# y_test = keras.utils.to_categorical(y_test, num_classes)
-
+# ==================================================== #
+# Model
+# ==================================================== #
 model = Sequential()
 
 model.add(Conv2D(32, (3, 3), padding='same', input_shape=x_train.shape[1:], data_format="channels_last"))
@@ -71,16 +83,52 @@ model.compile(loss='categorical_crossentropy',
 
 x_train = x_train.astype('float32')
 x_test = x_test.astype('float32')
-x_train /= 255
-x_test /= 255
+# x_train /= 255
+# x_test /= 255
 
+model.summary()  ## print model summary
+with open(Results_dir.joinpath("mode.json").as_posix(), "w") as fp:
+    fp.write(model.to_json())
+
+try:
+    from keras.utils import plot_model
+    plot_model(model, to_file=Results_dir.joinpath("model.png"), show_shapes=True)
+except Exception as e:
+    print(e)
+
+# ==================================================== #
+# Report Call Back
+# ==================================================== #
+
+SaveMode_filepath = Results_dir.joinpath("model.hd5")
+
+# Call Back 1 : save model
+cb_check = keras.callbacks.ModelCheckpoint(SaveMode_filepath.as_posix())
+
+# Call Back 1 : Tensor Board
+Log_dir = Results_dir.joinpath("logs")
+if (not Log_dir.exists()): Log_dir.mkdir()
+
+cb_tensorboard = keras.callbacks.TensorBoard(log_dir=Log_dir.as_posix(),
+                                             histogram_freq=0,
+                                             write_graph=True)
+
+# Call Back 1  CSV
+result_csv_path = Results_dir.joinpath("training.csv")
+cb_csvlogger = keras.callbacks.CSVLogger(result_csv_path.as_posix(), separator=',', append=False)
+
+
+# ==================================================== #
+# Training
+# ==================================================== #
 if not data_augmentation:
     print('Not using data augmentation.')
     model.fit(x_train, y_train,
               batch_size=batch_size,
               epochs=epochs,
               validation_data=(x_test, y_test),
-              shuffle=True)
+              shuffle=True,
+              callbacks=[cb_check, cb_tensorboard, cb_csvlogger])
 else:
     print('Using real-time data augmentation.')
     # This will do preprocessing and realtime data augmentation:
@@ -105,15 +153,19 @@ else:
                                      batch_size=batch_size),
                         steps_per_epoch=x_train.shape[0] // batch_size,
                         epochs=epochs,
-                        validation_data=(x_test, y_test))
+                        validation_data=(x_test, y_test),
+                        callbacks=[cb_check, cb_tensorboard, cb_csvlogger])
 
-    
-# evaluate model with sklearn
+
+# ==================================================== #
+# evaluate model
+# ==================================================== #
 predicted = model.predict(x_test, batch_size=batch_size, verbose=1)
 y_true = np.argmax(y_test, axis=1)
 y_pred = np.argmax(predicted, axis=1)
-target_names = ['No Sairen', 'On Sairen' ]
+target_names = ['No Sairen', 'On Sairen']
 
 print(sklearn.metrics.classification_report(y_true, y_pred, target_names=target_names, digits=3))
 from sklearn.metrics import confusion_matrix
+
 print(confusion_matrix(y_true, y_pred))
